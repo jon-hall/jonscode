47f5e02ec0c45f4798a9e10cfcfb5e15366fef1b
Cameron Smith <gitcontrol@outlook.com>
2017-02-28 22:57:16 +0000

    Added the most important ASCII art

--- index.php
+++ index.php
@@ -9,6 +9,23 @@ $stringTest = App\Objects\TestSuite::test(new App\Objects\TestString($files, 100
 
 echo "\n\n";
 
+echo <<<OUTPUT
+========================
+       _             
+      | |            
+      | | ___  _ __  
+  _   | |/ _ \| '_ \ 
+ | |__| | (_) | | | |
+  \____/ \___/|_| |_|
+                     
+                     
+ ** The favicon king **                     
+
+========================
+OUTPUT;
+
+echo "\n\n";
+
 print_r($regexTest);
 print_r($stringTest);
 
